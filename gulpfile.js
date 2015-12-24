0e7ee10689d528c648b3ac817f99c7ea0c6ed2bf
Steven Raden <stevengraden@gmail.com>
2015-12-24 09:06:37 -0800

    got gulp file setup to reload when any file changes. Uses port 8000 and proxies to 8008

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
-	
+gulp.task('default', ['browser-sync'], function() {
+
 });
 
 //static server
 gulp.task('browser-sync', function() {
-	browserSync.init({
-		server: {
-			baseDir: "./"
-		}
+	browserSync.init(null, {
+		proxy: "http://localhost:8008",
+		files: ["*"],
+		browser: "google chrome",
+		port: 8000
 	});
 });
